61b6532bdd1036f797bd2402db2ce57cfb3b1813
Alex <calexkwok@gmail.com>
2026-03-11 10:47:50 -0700

    revert date square to solid color instead of gradient

--- src/App.js
+++ src/App.js
@@ -13380,18 +13380,14 @@ function App() {
                           w-full aspect-square rounded-lg p-1 transition-all duration-200 relative select-none
                           ${!date ? 'invisible' : 'bg-white dark:bg-gray-700'}
                           ${hasUrgentEvent && !isSelected && !isInSelection ? 'ring-2 ring-red-500 shadow-lg shadow-red-200' : ''}
-                          ${isInSelection ? 'text-white shadow-lg scale-105 ring-2' : ''}
-                          ${isSelected && !isInSelection ? 'text-white shadow-lg scale-105' : ''}
-                          ${!isInSelection && !isSelected && isTodayDate && !hasUrgentEvent ? 'ring-2' : ''}
+                          ${isInSelection ? 'bg-gradient-to-br from-purple-400 to-indigo-400 text-white shadow-lg scale-105 ring-2 ring-purple-300' : ''}
+                          ${isSelected && !isInSelection ? 'bg-gradient-to-br from-purple-500 to-indigo-500 text-white shadow-lg scale-105' : ''}
+                          ${!isInSelection && !isSelected && isTodayDate && !hasUrgentEvent ? 'bg-gradient-to-br from-rose-100 to-purple-100 dark:from-rose-900/50 dark:to-purple-900/50 ring-2 ring-purple-400' : ''}
                           ${!isInSelection && !isSelected && !isTodayDate && !hasUrgentEvent ? 'text-gray-700 dark:text-gray-100 dark:hover:bg-gray-600' : ''}
                           ${hasUrgentEvent && !isSelected && !isInSelection ? 'bg-red-50 dark:bg-red-900/30' : ''}
                         `}
                         style={{
                           zIndex: 10,
-                          ...(isInSelection || isSelected ? themeSelectedSurfaceStyle : {}),
-                          ...(!isInSelection && !isSelected && isTodayDate && !hasUrgentEvent
-                            ? { ...themeTodaySurfaceStyle, borderWidth: 0, boxShadow: `inset 0 0 0 2px ${themeAccentBorder}` }
-                            : {}),
                           ...(!isInSelection && !isSelected && !isTodayDate && !hasUrgentEvent
                             ? { '--theme-hover-bg': themeAccentSofterBg }
                             : {}),
@@ -13464,14 +13460,12 @@ function App() {
                       onClick={() => handleDateTap(date)}
                       className={`
                         min-h-24 rounded-lg p-1.5 cursor-pointer transition-all duration-200 flex flex-col gap-1 relative
-                        ${isSelected ? 'text-white shadow-lg ring-2' : ''}
-                        ${!isSelected && isTodayDate ? 'ring-2' : ''}
+                        ${isSelected ? 'bg-gradient-to-br from-purple-500 to-indigo-500 text-white shadow-lg ring-2 ring-purple-300' : ''}
+                        ${!isSelected && isTodayDate ? 'bg-gradient-to-br from-rose-100 to-purple-100 dark:from-rose-900/50 dark:to-purple-900/50 ring-2 ring-purple-400' : ''}
                         ${!isSelected && !isTodayDate ? 'bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600' : ''}
                         ${hasUrgentEvent && !isSelected ? 'ring-2 ring-red-500' : ''}
                       `}
                       style={{
-                        ...(isSelected ? themeSelectedSurfaceStyle : {}),
-                        ...(!isSelected && isTodayDate ? { ...themeTodaySurfaceStyle, boxShadow: `inset 0 0 0 2px ${themeAccentBorder}` } : {}),
                         ...(!isSelected && !isTodayDate ? { '--theme-hover-bg': themeAccentSofterBg } : {}),
                       }}
                       >
